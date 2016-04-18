d399f0094b8dc32f67d13dfcfe1ee6eda06500f0
Mike Wielgosz <mike@sekwel.com>
2016-04-18 12:08:34 -0400

    Android.mk: Fix for AOSP build. Include Superuser module

--- Android.mk
+++ Android.mk
@@ -39,3 +39,16 @@ ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
     $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SUPERUSER_RC) $(SUPERUSER_MARKER)
 
 endif
+
+include $(CLEAR_VARS)
+
+LOCAL_PATH := $(my_path)/Superuser
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+LOCAL_PACKAGE_NAME := Superuser
+LOCAL_SRC_FILES := $(call all-java-files-under,src) $(call all-java-files-under,Widgets/Widgets/src)
+
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+LOCAL_AAPT_FLAGS := --extra-packages com.koushikdutta.widgets -S $(LOCAL_PATH)/Widgets/Widgets/res --auto-add-overlay --rename-manifest-package $(SUPERUSER_PACKAGE)
+
+include $(BUILD_PACKAGE)
+
